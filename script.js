2fd858d02b2a5b68cc88c1abb6b275c2eca43b68
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-18 15:05:32 +0100

    Add randomised weapon generator

--- script.js
+++ script.js
@@ -3,12 +3,26 @@
 // figering out when button was clicked
 
 function rockButton() {
-    console.log(document.getElementById("rock").value);
+    console.log('rock');
+    return 'rock';
 }
 function paperButton() {
-    console.log(document.getElementById("paper").value);
+    console.log('paper');
+    return 'paper';
 }
 function scissorsButton() {
-    console.log(document.getElementById("scissors").value);
+    console.log('scissors');
+    return 'scissors';
 }
+
+const weapon = ['rock', 'paper', 'scissors'];
+const randomWeapon = weapon[Math.floor( Math.random () * weapon.length)];
+console.log(randomWeapon);
 // })();
+
+/*
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+const random = Math.floor(Math.random() * months.length);
+console.log(random, months[random]);
+*/
