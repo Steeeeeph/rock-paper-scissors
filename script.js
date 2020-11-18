799d6d4153f8c8eee36506dd83d7adc95a8688f2
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-18 14:44:23 +0100

    Onclick events added

---
+++ script.js
@@ -0,0 +1,14 @@
+// (function(){
+
+// figering out when button was clicked
+
+function rockButton() {
+    console.log(document.getElementById("rock").value);
+}
+function paperButton() {
+    console.log(document.getElementById("paper").value);
+}
+function scissorsButton() {
+    console.log(document.getElementById("scissors").value);
+}
+// })();
