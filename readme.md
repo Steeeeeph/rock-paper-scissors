7978901edf16e3557782360545fdee76f0d21a87
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-19 10:38:38 +0100

    Adding readme, little debugging try-outs in JS

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Casino Royale: Rock, paper, scissors
+## "Gambling with JavaScript"
+A challenge to generate random strings and compare it to given input (by buttons).
+There is no styling done (yet), the focus for now is on making proper vanilla JavaScript code...
+
+---
+## Have a chance!
+[Rock, paper, scissors](https://steeeeeph.github.io/rock-paper-scissors/)
+
+---
+## Toolbox
++ HTML
++ JavaScript
+
+---
+## Rights
+Made by [Stephanie Van Bockhaven](https://github.com/Steeeeeph) for [Becode](www.becode.org) Ghent.
--- script.js
+++ script.js
@@ -1,28 +1,62 @@
 // (function(){
 
 // figering out when button was clicked
-
+let myWeapon;
 function rockButton() {
     console.log('rock');
-    return 'rock';
+    let myWeapon = 'rock';
 }
 function paperButton() {
     console.log('paper');
-    return 'paper';
+    myWeapon = 'paper';
 }
 function scissorsButton() {
     console.log('scissors');
-    return 'scissors';
+    myWeapon = 'scissors';
 }
 
-const weapon = ['rock', 'paper', 'scissors'];
-const randomWeapon = weapon[Math.floor( Math.random () * weapon.length)];
-console.log(randomWeapon);
+document.getElementById("run").addEventListener('click', function (event) {
+    event.preventDefault();
+// random weapon
+    const weapon = ['rock', 'paper', 'scissors'];
+    const randomWeapon = weapon[Math.floor( Math.random () * weapon.length)];
+    console.log(randomWeapon)
+// Conditions
+    if (randomWeapon === myWeapon) {
+        console.log('Draw');
+        document.getElementById("outcome").innerHTML = 'Draw';
+    }
+    else if (randomWeapon === 'rock' && myWeapon === 'paper') {
+        console.log('Win');
+        document.getElementById("outcome").innerHTML = 'Win';
+    }
+    else if (randomWeapon === 'paper' && myWeapon === 'rock') {
+        console.log('Lose');
+        document.getElementById("outcome").innerHTML = 'Lose';
+
+    }
+    else if (randomWeapon === 'scissors' && myWeapon === 'paper') {
+        console.log('Lose');
+        document.getElementById("outcome").innerHTML = 'Lose';
+
+    }
+    else if (randomWeapon === 'paper' && myWeapon === 'scissors') {
+        console.log('Win');
+        document.getElementById("outcome").innerHTML = 'Win';
+
+    }
+    else if (randomWeapon === 'rock' && myWeapon === 'scissors') {
+        console.log('Lose');
+        document.getElementById("outcome").innerHTML = 'Lose';
+
+    }
+    else if (randomWeapon === 'scissors' && myWeapon === 'rock') {
+        console.log('Win');
+        document.getElementById("outcome").innerHTML = 'Win';
+
+    } 
+});
+
+
 // })();
 
-/*
-const months = ["January", "February", "March", "April", "May", "June", "July"];
-
-const random = Math.floor(Math.random() * months.length);
-console.log(random, months[random]);
-*/
