e1dc1ec8ded511f37a8d6a6f013af4bdbdccbdb1
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-20 09:44:12 +0100

    Styling on the choice buttons

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 let myWeapon;
 function rockButton() {
     console.log('rock');
-    let myWeapon = 'rock';
+    myWeapon = 'rock';
 }
 function paperButton() {
     console.log('paper');
@@ -15,7 +15,7 @@ function scissorsButton() {
     myWeapon = 'scissors';
 }
 
-document.getElementById("run").addEventListener('click', function (event) {
+document.getElementById("run").addEventListener('click', function takeAchance (event) {
     event.preventDefault();
 // random weapon
     const weapon = ['rock', 'paper', 'scissors'];
@@ -25,36 +25,51 @@ document.getElementById("run").addEventListener('click', function (event) {
     if (randomWeapon === myWeapon) {
         console.log('Draw');
         document.getElementById("outcome").innerHTML = 'Draw';
+        document.body.style.backgroundColor = "yellow";
     }
     else if (randomWeapon === 'rock' && myWeapon === 'paper') {
         console.log('Win');
         document.getElementById("outcome").innerHTML = 'Win';
+        document.body.style.backgroundColor = "green";
     }
     else if (randomWeapon === 'paper' && myWeapon === 'rock') {
         console.log('Lose');
         document.getElementById("outcome").innerHTML = 'Lose';
-
+        document.body.style.backgroundColor = "red";
     }
     else if (randomWeapon === 'scissors' && myWeapon === 'paper') {
         console.log('Lose');
         document.getElementById("outcome").innerHTML = 'Lose';
-
+        document.body.style.backgroundColor = "red";
     }
     else if (randomWeapon === 'paper' && myWeapon === 'scissors') {
         console.log('Win');
         document.getElementById("outcome").innerHTML = 'Win';
-
+        document.body.style.backgroundColor = "green";
     }
     else if (randomWeapon === 'rock' && myWeapon === 'scissors') {
         console.log('Lose');
         document.getElementById("outcome").innerHTML = 'Lose';
-
+        document.body.style.backgroundColor = "red";
     }
     else if (randomWeapon === 'scissors' && myWeapon === 'rock') {
         console.log('Win');
         document.getElementById("outcome").innerHTML = 'Win';
-
+        document.body.style.backgroundColor = "green";
     } 
+// after a game: trying again
+/*
+function removeEvent () {
+    document.getElementById("run").removeEventListener('click', takeAchance(event));
+}
+    let result= confirm('Wanna try again?');
+    if(result) {
+       location.reload(console.clear());
+    }
+    else if (!result) {
+        document.getElementById("outcome").innerHTML = 'Maybe Another time then!';    
+        removeEvent();
+    }*/
 });
 
 
