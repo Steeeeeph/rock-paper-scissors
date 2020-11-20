6f88dfbcbffc1986fa9f71394eb33ac04b5a22b9
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-20 15:45:49 +0100

    Adding reset button

--- script.js
+++ script.js
@@ -24,52 +24,57 @@ document.getElementById("run").addEventListener('click', function takeAchance (e
 // Conditions
     if (randomWeapon === myWeapon) {
         console.log('Draw');
-        document.getElementById("outcome").innerHTML = 'Draw';
-        document.body.style.backgroundColor = "yellow";
+        document.getElementById("outcome").innerHTML = 'It\'s a Draw';
+        document.getElementById("result").style.backgroundColor = "yellow";
     }
     else if (randomWeapon === 'rock' && myWeapon === 'paper') {
         console.log('Win');
-        document.getElementById("outcome").innerHTML = 'Win';
-        document.body.style.backgroundColor = "green";
+        document.getElementById("outcome").innerHTML = 'You Win';
+        document.getElementById("result").style.backgroundColor = "green";
     }
     else if (randomWeapon === 'paper' && myWeapon === 'rock') {
         console.log('Lose');
-        document.getElementById("outcome").innerHTML = 'Lose';
-        document.body.style.backgroundColor = "red";
+        document.getElementById("outcome").innerHTML = 'You Lose';
+        document.getElementById("result").style.backgroundColor = "red";
     }
     else if (randomWeapon === 'scissors' && myWeapon === 'paper') {
         console.log('Lose');
-        document.getElementById("outcome").innerHTML = 'Lose';
-        document.body.style.backgroundColor = "red";
+        document.getElementById("outcome").innerHTML = 'You Lose';
+        document.getElementById("result").style.backgroundColor = "red";
     }
     else if (randomWeapon === 'paper' && myWeapon === 'scissors') {
         console.log('Win');
-        document.getElementById("outcome").innerHTML = 'Win';
-        document.body.style.backgroundColor = "green";
+        document.getElementById("outcome").innerHTML = 'You Win';
+        document.getElementById("result").style.backgroundColor = "green";
     }
     else if (randomWeapon === 'rock' && myWeapon === 'scissors') {
         console.log('Lose');
-        document.getElementById("outcome").innerHTML = 'Lose';
-        document.body.style.backgroundColor = "red";
+        document.getElementById("outcome").innerHTML = 'You Lose';
+        document.getElementById("result").style.backgroundColor = "red";
     }
     else if (randomWeapon === 'scissors' && myWeapon === 'rock') {
         console.log('Win');
-        document.getElementById("outcome").innerHTML = 'Win';
-        document.body.style.backgroundColor = "green";
+        document.getElementById("outcome").innerHTML = 'You Win';
+        document.getElementById("result").style.backgroundColor = "green";
     } 
 // after a game: trying again
-/*
-function removeEvent () {
-    document.getElementById("run").removeEventListener('click', takeAchance(event));
-}
-    let result= confirm('Wanna try again?');
+// /*
+// function removeEvent () {
+//     document.getElementById("run").removeEventListener('click', takeAchance(event));
+// }
+    let result = function afterRound() {
+    }
     if(result) {
-       location.reload(console.clear());
+        document.getElementById("run").innerHTML = 'Wanna play some more?';
+        document.getElementById("reset").style.display = 'flex';
     }
     else if (!result) {
+        location.reload(console.clear());
+
         document.getElementById("outcome").innerHTML = 'Maybe Another time then!';    
         removeEvent();
-    }*/
+    }
+    // */
 });
 
 
